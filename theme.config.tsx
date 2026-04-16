ca1aba94306cf0692f94acd2734e95ee3c1c23e8
Amanda Caraça <amanda.caraca@olos.com.br>
2026-04-16 13:06:20 -0300

    feat: initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Olos para Desenvolvedores</span>,
+  head: (
+    <>
+      <title>Olos para Desenvolvedores</title>
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="https://www.olos.com.br/wp-content/uploads/2020/03/cropped-android-icon-192x192-1-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="https://www.olos.com.br/wp-content/uploads/2020/03/cropped-android-icon-192x192-1-192x192.png"
+      />
+    </>
+  ),
+  search: {
+    placeholder: "Buscar...",
+  },
+  footer: {
+    text: "Olos Tecnologia e Sistemas LTDA.",
+  },
+  toc: {
+    title: "Nesta página",
+    extraContent: false,
+  },
+  feedback: {
+    content: "",
+  },
+  editLink: {
+    text: "",
+  },
+  gitTimestamp: "",
+  sidebar: {
+    defaultMenuCollapseLevel: -1,
+  },
+  i18n: [
+    { locale: "pt-BR", text: "Português BR" },
+    { locale: "es", text: "Español" },
+    { locale: "us", text: "English" },
+  ],
+};
+
+export default config;
